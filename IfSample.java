2c3d595b2dbd2b5e367ed6de67ec9044c2d7218e
satoshisuzuki <yousei.suzuki0416@gmail.com>
2025-07-12 17:01:49 +0900

    メッセージ

---
+++ IfSample.java
@@ -0,0 +1,16 @@
+public class IfSample {
+   public IfSample() {
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 75;
+      if (var1 >= 80) {
+         System.out.println("よくできました！");
+      } else if (var1 >= 60) {
+         System.out.println("合格です！");
+      } else {
+         System.out.println("不合格です");
+      }
+
+   }
+}
