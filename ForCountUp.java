67c897cadf4fa7c76c3a31598ae1816dd5637a9e
satoshisuzuki <yousei.suzuki0416@gmail.com>
2025-07-12 18:43:04 +0900

    Add ForCountUp.java and fix typo in println

---
+++ ForCountUp.java
@@ -0,0 +1,7 @@
+public class ForCountUp {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("カウント: " + i); // ←ここが正しい
+        }
+    }
+}
